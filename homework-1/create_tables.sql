bccc2d7172527e7c33f9cb5e3c1511e065271f48
ekaterina <sychevaee@icloud.com>
2024-03-11 09:03:27 +0700

    Запросы на создание таблиц в create_tables.sql; Коды на заполнение таблиц - в main.py

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,32 @@
 -- SQL-команды для создания таблиц
+
+
+--employees
+create table employees
+(
+employee_id int primary key,
+first_name varchar (30),
+last_name varchar (30),
+title varchar (30),
+birth_date date,
+notes text
+)
+
+--customers
+create table customers
+(
+customer_id varchar (10) primary key,
+company_name varchar(50),
+contact_name varchar (50)
+)
+
+
+--orders
+create table orders_data
+(
+order_id int primary key,
+customer_id varchar(10) REFERENCES customers(customer_id) NOT NULL,
+employee_id int REFERENCES employees(employee_id) NOT NULL,
+order_date date,
+ship_city varchar (30)
+)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,61 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="north",
+    user="postgres",
+    password="123123"
+)
+
+# -- Таблица employees
+# with open("north_data/employees_data.csv", 'r', newline='', encoding="UTF8") as file:
+#     employees = csv.reader(file)
+#     next(employees)
+#     with conn.cursor() as cur:
+#         for employee in employees:
+#             cur.execute(
+#                 "INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES (%s,%s, %s, %s, %s, %s)",
+#                 employee
+#             )
+#         cur.execute("SELECT * FROM employees")
+#         rows = cur.fetchall()
+#         conn.commit()
+#
+#     conn.close()
+
+
+# --Таблица customers
+# with open("north_data/customers_data.csv", 'r', newline='', encoding="UTF8") as file:
+#     customers = csv.reader(file)
+#     next(customers)
+#     with conn.cursor() as cur:
+#         for customer in customers:
+#             cur.execute(
+#                 "INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s)",
+#                 customer
+#             )
+#         cur.execute("SELECT * FROM customers")
+#         rows = cur.fetchall()
+#         conn.commit()
+#
+#     conn.close()
+
+
+# --Таблица orders
+with open("north_data/orders_data.csv", 'r', newline='', encoding="UTF8") as file:
+    orders = csv.reader(file)
+    next(orders)
+    with conn.cursor() as cur:
+        for order in orders:
+            cur.execute(
+                "INSERT INTO orders (order_id, customer_id, employee_id, order_date, ship_city) VALUES (%s, %s, %s, %s, %s)",
+                order
+            )
+        cur.execute("SELECT * FROM customers")
+        rows = cur.fetchall()
+        conn.commit()
+
+    conn.close()
